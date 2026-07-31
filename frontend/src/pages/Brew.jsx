b9784ce8249d8e6746d58bce35424bbb2bc7135d
Tanu Pundir <tpundir007@gmail.com>
2026-08-01 01:20:59 +0530

    Connect frontend to deployed backend

--- frontend/src/pages/Brew.jsx
+++ frontend/src/pages/Brew.jsx
@@ -12,7 +12,7 @@ import Simulator from "../components/Simulator";
 import SkillModal from "../components/SkillModal";
 import ClarificationModal from "../components/ClarificationModal";
 
-const API = "http://127.0.0.1:5000";
+const API = "https://promptea-final.onrender.com";
 const TOTAL_STEPS = 12;
 
 const obj = (v) => {
