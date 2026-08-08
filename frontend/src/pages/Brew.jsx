7f8fa5b7d90fc8979943f180b28993bbdee6def8
Tanu Pundir <tpundir007@gmail.com>
2026-08-08 10:16:25 +0530

    deployed

--- frontend/src/pages/Brew.jsx
+++ frontend/src/pages/Brew.jsx
@@ -12,7 +12,7 @@ import Simulator from "../components/Simulator";
 import SkillModal from "../components/SkillModal";
 import ClarificationModal from "../components/ClarificationModal";
 
-const API = "https://promptea-final.onrender.com";
+const API = "https://promptea-final.onrender.com/";
 const TOTAL_STEPS = 12;
 
 const obj = (v) => {
